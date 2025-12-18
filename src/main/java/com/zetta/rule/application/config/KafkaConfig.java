4833fa8ded877b35b84963fb9191c56ef6797f4b
KristianKakalov <kris.kakalov@gmail.com>
2025-12-18 23:13:00 +0200

    fix consumer factory

--- src/main/java/com/zetta/rule/application/config/KafkaConfig.java
+++ src/main/java/com/zetta/rule/application/config/KafkaConfig.java
@@ -60,7 +60,7 @@ public class KafkaConfig {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JacksonJsonDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
 
         return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
